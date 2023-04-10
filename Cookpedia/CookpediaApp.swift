ffede6972702ba8081cc296d6f53f903951b10c3
Rodrigo <rodcarcherd2128@gmail.com>
2023-04-09 18:28:27 -0600

    Initial Commit

---
+++ Cookpedia/CookpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookpediaApp.swift
+//  Cookpedia
+//
+//  Created by Rodrigo García Hernández on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CookpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
